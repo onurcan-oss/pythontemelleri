435cf37ad1566770ac853e108b6c4ba088201b38
onurcan-oss <onurcanakcay1@gmail.com>
2022-02-22 09:19:50 +0300

    Create numbers1.py

---
+++ numbers1.py
@@ -0,0 +1,20 @@
+#müşter, adı
+# müsteri soyadı
+#musteri adı + soydı 
+#müsteri cinsiyet
+#musteri tc kimlik 
+#musteri doğum yılı
+#musteri adres bilgisi 
+#olunan yıl
+#musteri yası
+
+
+musteri_adi = 'onurcan'
+musteri_soyadi = 'Akcay'
+musteriAdSoyad = musteri_adi +''+ musteri_soyadi
+mustericinsiyet = True #Erkek içindir. #false Kadın.
+musteriTcKimlik = '12345678910'
+musteriDogumYili = 1989
+musteriAdresi = 'İstanbul/Kadikoy'
+olunanYil = 2022
+musteriYasi = olunanYil -musteriDogumYili
