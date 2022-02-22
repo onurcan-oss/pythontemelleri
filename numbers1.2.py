051ec01721f1b32a65e68329ffee399785a0df96
onurcan-oss <onurcanakcay1@gmail.com>
2022-02-22 09:30:44 +0300

    Create numbers1.2.py

---
+++ numbers1.2.py
@@ -0,0 +1,10 @@
+#aşağıdaki siparişlerin toplam bilgisini hesaplayınız
+#sipariş1 =110 tl 
+#siparis2 = 1100.5 tl 
+#siparis3 = 356.95 tl 
+
+siparisoneFiyat = 110
+siparistwoFiyat = 1100.5 
+siparisthreeFiyat = 356.95
+
+toplamSiparisFiyati = siparisoneFiyat + siparistwoFiyat + siparisthreeFiyat
