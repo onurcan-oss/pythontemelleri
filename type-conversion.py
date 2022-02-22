83c2dfded2189072858bff7a98335068a40f2f1e
onurcan-oss <onurcanakcay1@gmail.com>
2022-02-22 11:10:31 +0300

    Create type-conversion.py

---
+++ type-conversion.py
@@ -0,0 +1,61 @@
+# x = input('1. Sayı: ')
+# y = input('2. Sayı: ')
+
+# print(type(x)) #tür dönüşümü yapılarak sayıların toplanması sağlandı tür dönüşümü yapılmasaydı ekranda string bir yazı oluşacaktı.
+# print(type(y))
+
+
+# toplam = int(x) +  int(y)
+# print(toplam)
+
+
+from cgitb import reset
+from unittest import result
+
+
+x = 5               #int
+y = 2.5             #float
+name = 'Çınar'      #str
+isOnline = True     #bool
+
+# print(type(x))
+# print(type(y))  #tür dönüşümü yapılacak
+# print(type(name))
+# print(type(isOnline))
+
+#int to float (int den float dönüşümü)
+
+# x = float(x)
+
+# print(x)
+
+# print(type(x))
+
+
+#float to int  (float dan int e dönüşüm)
+
+# y = int(y)
+
+# print(y)
+
+# print(type(y))
+
+
+
+# result = x + y
+# print(result)
+# print(type(result))
+
+
+#bool to str (bool dan string e dönüşüm)
+
+# isOnline = str(isOnline)
+# print(isOnline)
+# print(type(isOnline))
+
+
+#bool to int  (bool dan int e dönüşüm)
+
+# isOnline = int(isOnline)
+# print(isOnline)
+# print(type(isOnline))
