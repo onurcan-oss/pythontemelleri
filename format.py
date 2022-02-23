edc9fbdcc73938ed7769b78c48311867d6eb5636
onurcan-oss <onurcanakcay1@gmail.com>
2022-02-23 08:24:29 +0300

    Create format.py

---
+++ format.py
@@ -0,0 +1,11 @@
+name = 'onurcan'
+surname = 'Akcay'
+age = 20
+
+# print('My name is {} {}'.format(name,surname))
+# print('My name is {1} {0}'.format(name,surname))
+# print('My name is {s} {n}'.format(n = name,s= surname))
+# print('My name is {} {} and Im {} years old.'.format(name,surname,age))
+
+result = 200 / 500
+print('the result is {}'.format(result))
